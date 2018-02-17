716c6cc1793191b80c4cb69e7020ec1a4a85d663
GMLim <yagyu82@gmail.com>
2018-02-18 01:24:03 +0900

    PART 03 안드로이드 앱의 화면 구성하기

---
+++ chap3/3-3/LifeCycle/app/src/main/java/com/practice/sample/lifecycle/LifeCycleActivity.java
@@ -0,0 +1,43 @@
+package com.practice.sample.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LifeCycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
